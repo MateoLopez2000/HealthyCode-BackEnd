8f4a3507bb57e81eca52e3d81abea73e8326e3c1
MateoLopez2000 <lm67427572@gmail.com>
2021-05-10 19:19:10 -0400

    date as String

--- models/Movie.js
+++ models/Movie.js
@@ -4,7 +4,7 @@ const movieSchema = new Schema({
   name: { type: String, required: true },
   descripcion: { type: String, required: true },
   genero: { type: String, required: true },
-  fecha: { type: Date, required: true },
+  fecha: { type: String, required: true },
 });
 
 module.exports = mongoose.model('movies', movieSchema);
