119714c51b909583facc97adde8b47537449a593
MateoLopez2000 <lm67427572@gmail.com>
2021-05-13 18:43:46 -0400

    version for presentation

--- routes/movie.route.js
+++ routes/movie.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const route = express.Router();
 const MovieCtrl = require('../controllers/movie.controller');
 
+route.get('/movie', MovieCtrl.getList);
 route.post('/movie', MovieCtrl.MovieByName);
 route.post('/findmovie', MovieCtrl.findOnebyname);
 route.post('/findmoviefiltered', MovieCtrl.findOnebyfilter);
