690936627a37150bed8071c6351d3f41ce5b84ef
MateoLopez2000 <lm67427572@gmail.com>
2021-05-13 15:12:49 -0400

    fixed getMoviByName

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,61 +1,118 @@
-const MovieCtrl = {}
-const Movie = require('../models/Movie')
-MovieCtrl.getList = async (req, res) =>{
-   const Movies= await Movie.find()
-   console.log(Movies)
-   res.json (Movies)
-}
+const MovieCtrl = {};
+const Movie = require('../models/Movie');
+
+MovieCtrl.MovieByName = (req, res) => {
+  console.log(req.body.name);
+  Movie.findOne({
+    name: req.body.name,
+  }).exec((err, mov) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    if (!mov) {
+      return res.status(404).send({ message: 'Movie Not found.' });
+    }
+    res.status(200).send({
+      id: mov._id,
+      name: mov.name,
+      descripcion: mov.descripcion,
+      genero: mov.genero,
+      fecha: mov.fecha,
+      imagen: mov.imagen,
+    });
+  });
+};
+
+MovieCtrl.getMovieByName = async (req, res) => {
+  const movie = new User({
+    name: movie.name,
+    descripcion: movie.descripcion,
+    genero: movie.genero,
+    fecha: movie.fecha,
+    imagen: movie.imagen,
+  });
+  Movie.find({
+    name: {
+      $regex: req.body.name,
+      $options: 'i',
+    },
+  }).exec((err, movie) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    if (!movie) {
+      return res.status(404).send({ message: 'Movie Not found.' });
+    }
+    res.status(200).send({
+      id: movie._id,
+      name: movie.name,
+      descripcion: movie.descripcion,
+      genero: movie.genero,
+      fecha: movie.fecha,
+      imagen: movie.imagen,
+    });
+  });
+};
+
+MovieCtrl.getList = async (req, res) => {
+  const Movies = await Movie.find();
+  console.log(Movies);
+  res.json(Movies);
+};
 
 MovieCtrl.findOnebyname = (req, res) => {
-   Movie.find({
-      name: {
-         $regex: req.body.name, $options: 'i'
-      },
-   }).exec((err, movie) => {
-     if (err) {
-       res.status(500).send({ message: err });
-       return;
-     }
-     if (!movie) {
-       return res.status(404).send({ message: "Movie Not found." });
-     }
-     res.status(200).send({
-       id: movie._id,
-       name: movie.name,
-       descripcion: movie.descripcion,
-       genero: movie.genero,
-       fecha: movie.fecha,
-       imagen: movie.imagen,
-     });
-   });
- };
- MovieCtrl.findOnebyfilter = (req, res) => {
-   Movie.find({
-      fecha: {
-         $gte: req.body.stardate,
-         $lt: req.body.enddate,
-          },
-      name: {
-         $regex: req.body.name, $options: 'i'
-      },
-   })
-   .exec((err, movie) => {
-     if (err) {
-       res.status(500).send({ message: err });
-       return;
-     }
-     if (!movie) {
-       return res.status(404).send({ message: "Movie Not found." });
-     }
-     res.status(200).send({
-       id: movie._id,
-       name: movie.name,
-       descripcion: movie.descripcion,
-       genero: movie.genero,
-       fecha: movie.fecha,
-       imagen: movie.imagen,
-     });
-   });
- };
+  Movie.find({
+    name: {
+      $regex: req.body.name,
+      $options: 'i',
+    },
+  }).exec((err, movie) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    if (!movie) {
+      return res.status(404).send({ message: 'Movie Not found.' });
+    }
+    res.status(200).send({
+      id: movie._id,
+      name: movie.name,
+      descripcion: movie.descripcion,
+      genero: movie.genero,
+      fecha: movie.fecha,
+      imagen: movie.imagen,
+    });
+  });
+};
+MovieCtrl.findOnebyfilter = (req, res) => {
+  Movie.find({
+    fecha: {
+      $gte: req.body.stardate,
+      $lt: req.body.enddate,
+    },
+    name: {
+      $regex: req.body.name,
+      $options: 'i',
+    },
+  }).exec((err, movie) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    if (!movie) {
+      return res.status(404).send({ message: 'Movie Not found.' });
+    }
+    res.status(200).send({
+      id: movie._id,
+      name: movie.name,
+      descripcion: movie.descripcion,
+      genero: movie.genero,
+      fecha: movie.fecha,
+      imagen: movie.imagen,
+    });
+  });
+};
 
-module.exports = MovieCtrl
+module.exports = MovieCtrl;
--- routes/movie.route.js
+++ routes/movie.route.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const route = express.Router()
-const MovieCtrl =  require('../controllers/movie.controller')
-route.get('/movie', MovieCtrl.getList)
-route.get('/findmovie', MovieCtrl.findOnebyname)
-route.get('/findmoviefiltered', MovieCtrl.findOnebyfilter)
-module.exports = route
+const route = express.Router();
+const MovieCtrl = require('../controllers/movie.controller');
+
+route.post('/movie', MovieCtrl.MovieByName);
+route.post('/findmovie', MovieCtrl.findOnebyname);
+route.post('/findmoviefiltered', MovieCtrl.findOnebyfilter);
+module.exports = route;
